2406b8fa382306ec89f7eb256190d13e6c7af552
ashkan <ashkanaz2828@gmail.com>
2025-06-27 16:09:01 +0330

    chore: update todos

--- todos.ts
+++ todos.ts
@@ -8,43 +8,141 @@
     //TODO : use widePoster extracted from sources
     //TODO : implement widePosters as array
 
+    //TODO : check fresh deploy/start
+    //TODO : re check mongodb indexes
+
+    //TODo : refactor 'searchTools' -> getLinks
+
+    //--------------------------------------------
+    //--------------------------------------------
+
+    //TODO : add the new crawler to docker-compose
+    //TODO : upload docker image
+    //TODO : reduce docker image size
+    //TODO : make docker image
+
+    //TODO : reduce docker image size
+    //TODO : build image
+    //TODO : use 'dive' to check docker image
+    //TODO : use https://elysiajs.com/patterns/deploy
+    //TODO : use bun compile
+    //TODO : use lighter package for 'prisma'
+    //TODO : remove package 'sharp'
+
+    //TODO : **********************************
+    //TODO : test
+    //TODO : test crawler domain change detector
+    //TODO : test crawler
+    //TODO : test title insert/update
+    //TODO : test providers
+    //TODO : test repo layer
+    //TODO : test extractor functions
+    //TODO : **********************************
+
+    //TODO : check crawler manual pause for duration works after restart
+
+    //TODO : fix update detector, reduce posts in telegramBot
+
+    //todo : use supabase instead of self-host postgresql
+    //todo : remove the need for postgresql database
+    //TODO : fix postgres docker image error
+    //TODO : fix prisma error
+
+    //TODO : CHANGED :
+    //TODO : CHANGED :
+    //TODO : CHANGED : crawler status ( pauseData,  )
+
+    //TODO : re check project structure
+
+    //TODO : add their dependencies into devDependencies
+    //TODO : add 'sample' section into ignoreList
+
+    //TODO : update dependencies
+    //TODO : remove dependencies
+    //TODO : check dependencies
+
+    //TODO :
+    //TODO : change the way of handling blurGHash and thumbnail generation
+    //TODO : check resource needed for self-host mongodb
+    //TODO : check casandara db
+    //TODO : more dynamic db backup service
+    //TODO : refactor 'goChat' and remove it and rabbitMQ
+    //TODO : add source 'donyayeserial'
+    //TODO : add source 'movielandz'
+    //TODO : add source 'hexdl'
+
+    //TODO : better youtube trailer crawler job
+
+    //TODO : better crawler log save
+    //TODO : better server analysis save
+
+    //TODO :
+    //TODO : save more info on badPoster|badSummary|... generation
+    //TODO : add flag for gc call in crawler pause
+    //TODO : refactor crawler warnings
+    //TODO : refactor configs
+    //TODO : refactor source configs
+    //TODO : refactor crawler status management
+    //TODO : better server/crawler resource usage detector
+
     //TODO : refactor : torrent
     //TODO : refactor : admin panel
     //TODO : refactor : telegramBot
+    //todo : refactor : go-mail
     //TODO : refactor : user api
     //TODO : refactor : movie api
     //TODO : refactor : crawler
 
-    //TODO : option to choose between secure or weak authentication
-    //TODO : add new simple method for authentication
-    //TODO : refactor crawler
-    //TODO : use typescript + bun/fastify for lighter app
-    //TODO : separate crawler into new project
-    //TODO : 'axios' -> 'upFetch', 'kai', 'oFetch'
+    //TODO : check usage of << new Date(0) >>
 
-    //TODO : use profiler
-    //TODO : reduce cpu usage
-    //TODO : reduce memory usage
-    //TODO : update dependencies
-    //TODO : update node in dockerfile
-    //TODO : update node
+    //TODO : add db config for concurrency in 'addFileSizeToDownloadLinks'
+    //TODO : add db config for disabling s3 storage
+    //TODO : add db config for timeout between retry in 'searchTools' -> getLinks
+    //TODO : add db config for crawler concurrency on idle server state
 
-    //TODO : add redis cache
-    //TODO : add postgres
-    //TODO : add mongodb
-    //TODO : add sentry
-    //TODO : better error handling
+    //TODO : reduce static configs
+    //TODO : add dynamic config for 'PersonAndCharacter'
+    //TODO : move env config to db_config
 
-    //TODO : optimize project
-    //TODO : optimize cheerio
-    //TODO : optimize crawler
-    //TODO : update node/bun
-    //TODO : update dependencies
+    //TODO : send movies update notification in movie-api
+    //TODO : create blurHash in movie-api
+    //TODO : create thumbnail in movie-api
+    //TODO : remove usage of rabbitmq in crawler
+
     //TODO : refactor admin auth
     //TODO : refactor api validation
     //TODO : refactor admin apis
+    //TODO : option to choose between 'secure' or 'weak' authentication
+    //TODO : add new simple method for authentication
+
+    //TODO : 'axios' -> 'upFetch', 'kai', 'oFetch'
+
+    //TODO : wait for 'api' project in startup
+
+    //TODO : check intervals
+    //TODO : ask cursor about improvements
+    //TODO : use profiler
+    //TODO : reduce cpu usage
+    //TODO : reduce memory usage
+    //TODO : optimize project
+    //TODO : optimize cheerio
+    //TODO : optimize crawler
+
+    //TODO : check dependencies
+    //TODO : update dependencies
+
     //TODO : new README.md
-    //TODO : movie to bun + its httpServer
-    //TODO : new docker image
-    //TODO : refactor all section of project
-    //TODO : move code
+    //TODO : add swagger
+
+    //TODO : crawler logger
+    //TODO : logger
+    //TODO : better error handling
+
+    //TODO : add prisma
+    //TODO : add postgres
+
+    //TODO : add sentry
+
+    //TODO : add redis cache
+    //TODO : add redis
+
